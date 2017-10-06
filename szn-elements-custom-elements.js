1cdfac1baa3216fe2b1d3fa4ed03adef12f03db9
Martin Jurča <martin.jurca@firma.seznam.cz>
2017-10-06 10:52:09 +0200

    added documentation

--- szn-elements-custom-elements.js
+++ szn-elements-custom-elements.js
@@ -2,6 +2,15 @@
 ;(global => {
   const SznElements = global.SznElements = global.SznElements || {}
 
+  /**
+   * Registers the provided custom element with the runtime. This will ensure that any occurrence of the custom element
+   * in the DOM will be paired with the provided functionality.
+   *
+   * This implementation relies on the custom elements API, see http://mdn.io/Custom_Elements for more details.
+   *
+   * @param {string} elementName The DOM name of the custom element. This should be prefixed by "szn-".
+   * @param {function(HTMLElement, ?HTMLElement)} elementClass The class representing the custom element.
+   */
   SznElements.registerElement = (elementName, elementClass) => {
     customElements.define(elementName, class extends HTMLElement {
       constructor() {
--- szn-elements.js
+++ szn-elements.js
@@ -1,9 +1,15 @@
 'use strict'
 ;(global => {
-
   const SznElements = global.SznElements = global.SznElements || {}
   const registeredElements = {}
 
+  /**
+   * Initializes the runtime. This method should be called (if defined) whenever a new element is defined. Elements
+   * that have been defined since the last call will be initialized in the runtime so they can be used in the browser.
+   *
+   * The method has no effect if the element-to-browser registration runtime has not been loaded yet or no new elements
+   * were defined since the last call.
+   */
   SznElements.init = () => {
     if (!SznElements.registerElement) {
       return
@@ -15,6 +21,15 @@
     }
   }
 
+  /**
+   * Registers the provided callback to be triggered whenever any of the specified attributes change on the provided
+   * element. Changes to other attributes will be ignored.
+   *
+   * @param {HTMLElement} element The HTML element on which the attribute modifications are to be observed.
+   * @param {Array<string>} attributes The names of attributes that should be observed.
+   * @param {function(MutationRecord)} callback Callback executed whenever any of the specified attributes is modified.
+   * @return {function(): void} A function to disable the observing of the attributes. Use this to clean up.
+   */
   SznElements.observeAttributes = (element, attributes, callback) => {
     const observer = new MutationObserver(callback)
     observer.observe(element, {
@@ -25,6 +40,12 @@
     return () => observer.disconnect()
   }
 
+  /**
+   * Returns an iterator that yields all so-far registered custom elements.
+   *
+   * @return {Iterator<[string, function(HTMLElement, ?HTMLElement)]>} Iterator that yields the DOM tag name and the
+   *         custom element class tuple for each so-far registered custom element.
+   */
   function *getUnregisteredElements() {
     for (const key of Object.keys(SznElements)) {
       if (key.includes('-') && !registeredElements[key]) {
